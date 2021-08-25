cb63ec6d259523ec592bbc016a6847f8336452b7
klooke <klookegames@hotmail.com>
2021-08-25 07:45:33 -0300

    Atualização diretório.
    1. Alterado nome do diretório.

--- src/PlayerController.java
+++ src/PlayerController.java
@@ -15,9 +15,8 @@ public class PlayerController implements KeyListener
 		int key = e.getKeyCode();
 		
 		if(key == KeyEvent.VK_LEFT || key == KeyEvent.VK_A) 
-			player.moveX(Game.DIR_LEFT);
-		
-		if(key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_D)
+			player.moveX(Game.DIR_LEFT);		
+		else if(key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_D)
 			player.moveX(Game.DIR_RIGHT);		
 	}
 
